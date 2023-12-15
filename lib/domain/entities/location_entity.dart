231b25e9e45618b2dc2b363d5f15d8f6e5951634
Gabriel Santos <gabrielsantostca@gmail.com>
2023-12-14 22:57:47 -0300

    feat: changed the class to extend from item but not receive the locationId

--- lib/domain/entities/location_entity.dart
+++ lib/domain/entities/location_entity.dart
@@ -1,11 +1,9 @@
-class LocationEntity {
-  final String name;
-  final String? parentId;
-  final String id;
+import 'package:tractian_challenge/domain/entities/abstract_classes/item.dart';
 
+class LocationEntity extends Item {
   LocationEntity({
-    required this.name,
-    required this.parentId,
-    required this.id,
-  });
+    required super.name,
+    required super.parentId,
+    required super.id,
+  }) : super(locationId: null);
 }
