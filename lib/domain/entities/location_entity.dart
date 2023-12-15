25cdd177a021b2d03e0f8e5da1b463996385d855
Gabriel Santos <gabrielsantostca@gmail.com>
2023-12-14 22:05:33 -0300

    feat: created a location_entity

---
+++ lib/domain/entities/location_entity.dart
@@ -0,0 +1,11 @@
+class LocationEntity {
+  final String name;
+  final String? parentId;
+  final String id;
+
+  LocationEntity({
+    required this.name,
+    required this.parentId,
+    required this.id,
+  });
+}
