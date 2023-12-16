6d4842c05228b31cb85649d46c7534437835db8d
Gabriel Santos <gabrielsantostca@gmail.com>
2023-12-16 15:36:15 -0300

    feat: created new constants for the application

--- lib/core/helpers/constansts.dart
+++ lib/core/helpers/constansts.dart
@@ -1,1 +1,6 @@
 const String sampleDataDir = './assets/sample_data';
+const String assetFileName = 'assets.json';
+const String locationsFileName = 'locations.json';
+const String locationType = 'location';
+const String assetType = 'asset';
+const String componentType = 'component';
