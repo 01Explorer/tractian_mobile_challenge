c15e0b3129de11b7a281cac748f0cda80c7500a9
Gabriel Santos <gabrielsantostca@gmail.com>
2023-12-16 15:37:14 -0300

    feat: implemented a new method for the getCompanyTreeComponents useacas

--- lib/data/datasources/local/local_datasource_impl.dart
+++ lib/data/datasources/local/local_datasource_impl.dart
@@ -1,9 +1,11 @@
+import 'dart:convert';
 import 'dart:io';
 
 import 'package:dartz/dartz.dart';
 import 'package:tractian_challenge/core/exceptions/custom_exceptions.dart';
 
 import 'package:tractian_challenge/core/exceptions/failure_exception.dart';
+import 'package:tractian_challenge/core/helpers/constansts.dart';
 import 'package:tractian_challenge/core/helpers/extensions.dart';
 
 import 'local_datasource.dart';
@@ -18,5 +20,46 @@ class LocalDataSourceImpl implements LocalDataSource {
       throw const CompanyException(message: 'No Companies Found');
     }
     return Future.value(Right(namesList));
+  }
+
+  @override
+  Future<Either<Failure, List<Map<String, dynamic>>>> getCompanyTreeComponents(
+      {required String dirPath}) async {
+    try {
+      final listOfLocations =
+          await _fileContentToMap(dirPath: '$dirPath/$locationsFileName');
+      final listOfAssets =
+          await _fileContentToMap(dirPath: '$dirPath/$assetFileName');
+      for (final location in listOfLocations) {
+        location['type'] = locationType;
+      }
+
+      for (final asset in listOfAssets) {
+        if (asset.isComponent) {
+          asset['type'] = componentType;
+        } else {
+          asset['type'] = assetType;
+        }
+      }
+
+      return Right(listOfLocations..addAll(listOfAssets));
+    } catch (e) {
+      return Left(TreeFailure(message: e.toString()));
+    }
+  }
+
+  Future<List<Map<String, dynamic>>> _fileContentToMap(
+      {required String dirPath}) async {
+    final locationsFile = File(dirPath);
+    final locationsonFileContent = await locationsFile.readAsString();
+    return decodeJsonList(locationsonFileContent);
+  }
+
+  List<Map<String, dynamic>> decodeJsonList(String jsonList) {
+    final List<Map<String, dynamic>> decodedList =
+        (jsonDecode(jsonList) as List)
+            .map((e) => e as Map<String, dynamic>)
+            .toList();
+    return decodedList;
   }
 }
