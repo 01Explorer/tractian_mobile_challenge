62c9c1ed9e36875a061c31c9e6412565daac5173
Gabriel Santos <gabrielsantostca@gmail.com>
2023-12-15 23:26:18 -0300

    feat: implemented the company_repository

---
+++ lib/data/repositories/company_repository_impl.dart
@@ -0,0 +1,26 @@
+import 'package:dartz/dartz.dart';
+import 'package:tractian_challenge/core/exceptions/failure_exception.dart';
+import 'package:tractian_challenge/data/datasources/local/local_datasource.dart';
+import 'package:tractian_challenge/domain/entities/company_entity.dart';
+import 'package:tractian_challenge/domain/repositories/company_repository.dart';
+
+class CompanyRepositoryImpl implements CompanyRepository {
+  final LocalDataSource _localDataSource;
+
+  CompanyRepositoryImpl(this._localDataSource);
+  @override
+  Future<Either<Failure, List<CompanyEntity>>> fetchCompanies() async {
+    try {
+      final result = await _localDataSource.fetchCompanies();
+      final companiesList = result.fold(
+        (l) => null,
+        (r) => r,
+      );
+      return Right(companiesList!
+          .map((e) => CompanyEntity(name: e, rootComponents: []))
+          .toList());
+    } catch (e) {
+      return Left(CompanyFailure(message: e.toString()));
+    }
+  }
+}
