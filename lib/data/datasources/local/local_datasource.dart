cc7e632f967b3c10934deba8e72bcb65cd3caf28
Gabriel Santos <gabrielsantostca@gmail.com>
2023-12-16 00:07:45 -0300

    feat: added a path param to the fetchCompanies method on local_datasource

--- lib/data/datasources/local/local_datasource.dart
+++ lib/data/datasources/local/local_datasource.dart
@@ -2,5 +2,6 @@ import 'package:dartz/dartz.dart';
 import 'package:tractian_challenge/core/exceptions/failure_exception.dart';
 
 abstract class LocalDataSource {
-  Future<Either<Failure, List<String>>> fetchCompanies();
+  Future<Either<Failure, List<String>>> fetchCompanies(
+      {required String dirPath});
 }
