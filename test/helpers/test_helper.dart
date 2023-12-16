255f308d2282ce23ebbdaebc05386f3e226ccbf9
Gabriel Santos <gabrielsantostca@gmail.com>
2023-12-15 22:25:35 -0300

    test: generated the mock for the companyRepository

---
+++ test/helpers/test_helper.dart
@@ -0,0 +1,7 @@
+import 'package:mockito/annotations.dart';
+import 'package:tractian_challenge/domain/repositories/company_repository.dart';
+
+@GenerateMocks([
+  CompanyRepository,
+])
+void main() {}
