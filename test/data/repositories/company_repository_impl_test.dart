d62a0dcb7577252b4ac9d2c45e2a7745a04a3e83
Gabriel Santos <gabrielsantostca@gmail.com>
2023-12-16 00:10:02 -0300

    test: adjusted the test following the new interface to LocalDataSource

--- test/data/repositories/company_repository_impl_test.dart
+++ test/data/repositories/company_repository_impl_test.dart
@@ -3,6 +3,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 import 'package:tractian_challenge/core/exceptions/custom_exceptions.dart';
 import 'package:tractian_challenge/core/exceptions/failure_exception.dart';
+import 'package:tractian_challenge/core/helpers/constansts.dart';
 import 'package:tractian_challenge/data/datasources/local/local_datasource.dart';
 import 'package:tractian_challenge/data/repositories/company_repository_impl.dart';
 import 'package:tractian_challenge/domain/entities/company_entity.dart';
@@ -22,7 +23,7 @@ void main() {
   test(
       'If calling fetch companies is successful should return a list of CompanyEntity',
       () async {
-    when(mockLocalDataSource.fetchCompanies()).thenAnswer(
+    when(mockLocalDataSource.fetchCompanies(dirPath: sampleDataDir)).thenAnswer(
         (_) async => const Right(['Company 1', 'Company 2', 'Company 3']));
 
     final result = await companyRepository.fetchCompanies();
@@ -31,8 +32,8 @@ void main() {
   });
 
   test('If calling fetch companies fails should', () async {
-    when(mockLocalDataSource.fetchCompanies())
-        .thenThrow(CompanyException(message: 'There are no companies'));
+    when(mockLocalDataSource.fetchCompanies(dirPath: sampleDataDir))
+        .thenThrow(const CompanyException(message: 'There are no companies'));
 
     final result = await companyRepository.fetchCompanies();
     result.fold(
