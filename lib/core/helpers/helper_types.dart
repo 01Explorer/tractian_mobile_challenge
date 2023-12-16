36b41bff0795393ccf51f9395a444f32d29cf8d1
Gabriel Santos <gabrielsantostca@gmail.com>
2023-12-16 13:24:40 -0300

    feat: added factory methods for both enums

--- lib/core/helpers/helper_types.dart
+++ lib/core/helpers/helper_types.dart
@@ -1,11 +1,25 @@
 enum SensorType {
   energy,
-  vibration,
+  vibration;
+
+  factory SensorType.fromString(String type) {
+    return switch (type) {
+      'energy' => SensorType.energy,
+      _ => SensorType.vibration,
+    };
+  }
 }
 
 enum SensorStatus {
   alert,
-  operating,
+  operating;
+
+  factory SensorStatus.fromString(String status) {
+    return switch (status) {
+      'alert' => SensorStatus.alert,
+      _ => SensorStatus.operating,
+    };
+  }
 }
 
 typedef Sensor = ({SensorType sensorType, SensorStatus sensorStatus});
