d88a33175055ed2397a706006986e113e8295b37
Gabriel Santos <gabrielsantostca@gmail.com>
2023-12-16 00:46:52 -0300

    feat: added getCompanyTreeComponents method to the repository interface

--- lib/domain/repositories/company_repository.dart
+++ lib/domain/repositories/company_repository.dart
@@ -2,6 +2,10 @@ import 'package:dartz/dartz.dart';
 import 'package:tractian_challenge/core/exceptions/failure_exception.dart';
 import 'package:tractian_challenge/domain/entities/company_entity.dart';
 
+import '../entities/abstract_classes/item.dart';
+
 abstract class CompanyRepository {
   Future<Either<Failure, List<CompanyEntity>>> fetchCompanies();
+  Future<Either<Failure, List<Item>>> getCompanyTreeComponents(
+      {required CompanyEntity companyEntity});
 }
