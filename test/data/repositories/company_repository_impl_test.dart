96bb8feb91b67cf093b703f9c17399e97f4a8250
Gabriel Santos <gabrielsantostca@gmail.com>
2023-12-15 23:27:58 -0300

    test: developed tests to validate the company_repository implementation

---
+++ test/data/repositories/company_repository_impl_test.dart
@@ -0,0 +1,41 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:tractian_challenge/core/exceptions/custom_exceptions.dart';
+import 'package:tractian_challenge/core/exceptions/failure_exception.dart';
+import 'package:tractian_challenge/data/datasources/local/local_datasource.dart';
+import 'package:tractian_challenge/data/repositories/company_repository_impl.dart';
+import 'package:tractian_challenge/domain/entities/company_entity.dart';
+import 'package:tractian_challenge/domain/repositories/company_repository.dart';
+
+import '../../helpers/test_helper.mocks.dart';
+
+void main() {
+  late final LocalDataSource mockLocalDataSource;
+  late final CompanyRepository companyRepository;
+
+  setUpAll(() {
+    mockLocalDataSource = MockLocalDataSource();
+    companyRepository = CompanyRepositoryImpl(mockLocalDataSource);
+  });
+
+  test(
+      'If calling fetch companies is successful should return a list of CompanyEntity',
+      () async {
+    when(mockLocalDataSource.fetchCompanies()).thenAnswer(
+        (_) async => const Right(['Company 1', 'Company 2', 'Company 3']));
+
+    final result = await companyRepository.fetchCompanies();
+    result.fold(
+        (l) => expect(l, isNull), (r) => expect(r, isA<List<CompanyEntity>>()));
+  });
+
+  test('If calling fetch companies fails should', () async {
+    when(mockLocalDataSource.fetchCompanies())
+        .thenThrow(CompanyException(message: 'There are no companies'));
+
+    final result = await companyRepository.fetchCompanies();
+    result.fold(
+        (l) => expect(l, isA<CompanyFailure>()), (r) => expect(r, isNull));
+  });
+}
