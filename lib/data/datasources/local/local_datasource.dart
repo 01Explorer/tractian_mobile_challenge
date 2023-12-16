b11520210899816442571f7005e507bdbc455c59
Gabriel Santos <gabrielsantostca@gmail.com>
2023-12-16 13:30:00 -0300

    feat: added the new usacase method to the local_datasource interface

--- lib/data/datasources/local/local_datasource.dart
+++ lib/data/datasources/local/local_datasource.dart
@@ -4,4 +4,6 @@ import 'package:tractian_challenge/core/exceptions/failure_exception.dart';
 abstract class LocalDataSource {
   Future<Either<Failure, List<String>>> fetchCompanies(
       {required String dirPath});
+  Future<Either<Failure, List<Map<String, dynamic>>>> getCompanyTreeComponents(
+      {required String dirPath});
 }
