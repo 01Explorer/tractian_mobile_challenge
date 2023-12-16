e4dc19756d5784f29c92e8cf9cc69befdcd26bd9
Gabriel Santos <gabrielsantostca@gmail.com>
2023-12-16 15:36:30 -0300

    feat: created a new Map extension to make easier to test if it is Component or asset

--- lib/core/helpers/extensions.dart
+++ lib/core/helpers/extensions.dart
@@ -3,3 +3,8 @@ import 'dart:io';
 extension FileExtension on FileSystemEntity {
   String get name => path.split('\\').last;
 }
+
+extension MapExtension on Map<String, dynamic> {
+  bool get isComponent => this['status'] != null;
+  bool get isAsset => !isComponent;
+}
