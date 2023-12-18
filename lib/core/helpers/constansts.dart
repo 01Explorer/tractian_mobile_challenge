2dfcc2b652df027250f041e3377a37746755bb7d
Gabriel Santos <gabrielsantostca@gmail.com>
2023-12-18 20:40:32 -0300

    feat: added constants to the file

--- lib/core/helpers/constansts.dart
+++ lib/core/helpers/constansts.dart
@@ -1,6 +1,18 @@
-const String sampleDataDir = './assets/sample_data';
+import 'package:flutter/material.dart';
+
+const String sampleDataDir = 'assets/sample_data';
+const String companiesFilePath = '$sampleDataDir/companies.txt';
 const String assetFileName = 'assets.json';
 const String locationsFileName = 'locations.json';
 const String locationType = 'location';
 const String assetType = 'asset';
 const String componentType = 'component';
+
+//Colors
+const Color darkerColor = Color(0xff17192D);
+const Color companiesColors = Color(0xff0072AD);
+
+//assets
+const String companyIcon = 'assets/images/company_icon.png';
+const String locationIcon = 'assets/images/location_icon.png';
+const String tractianLog = 'assets/images/logo.png';
