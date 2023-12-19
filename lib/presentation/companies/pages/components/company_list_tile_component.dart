01ad9635d1cb3aa108427bfbeb525176fcbe4898
Gabriel Santos <gabrielsantostca@gmail.com>
2023-12-18 21:01:08 -0300

    refactor: changed onTap behavior

--- lib/presentation/companies/pages/components/company_list_tile_component.dart
+++ lib/presentation/companies/pages/components/company_list_tile_component.dart
@@ -16,41 +16,47 @@ class CompanyListTileComponent extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(bottom: 8.0),
-      child: ConstrainedBox(
-        constraints: const BoxConstraints.expand(height: 100),
-        child: Stack(
-          children: [
-            Card(
-              elevation: 16,
-              color: companiesColors,
-              child: Align(
-                alignment: Alignment.center,
-                child: ListTile(
-                  onTap: () {
-                    Navigator.of(context).push(MaterialPageRoute(
-                        builder: (_) => TreeView(company: company)));
-                  },
-                  leading: Image.asset(companyIcon),
-                  title: Text(
-                    company.name,
-                    style: Theme.of(context).textTheme.bodyMedium,
-                  ),
-                ),
-              ),
+      child: InkWell(
+        onTap: () => Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => TreeView(
+              company: company,
             ),
-            Align(
-              alignment: Alignment.bottomRight,
-              child: Padding(
-                padding: const EdgeInsets.all(16),
-                child: Opacity(
-                  opacity: 0.3,
-                  child: Image.asset(
-                    tractianLog,
+          ),
+        ),
+        child: ConstrainedBox(
+          constraints: const BoxConstraints.expand(height: 100),
+          child: Stack(
+            children: [
+              Card(
+                elevation: 16,
+                color: companiesColors,
+                child: Align(
+                  alignment: Alignment.center,
+                  child: ListTile(
+                    leading: Image.asset(companyIcon),
+                    title: Text(
+                      company.name,
+                      style: Theme.of(context).textTheme.bodyMedium,
+                    ),
                   ),
                 ),
               ),
-            )
-          ],
+              Align(
+                alignment: Alignment.bottomRight,
+                child: Padding(
+                  padding: const EdgeInsets.all(16),
+                  child: Opacity(
+                    opacity: 0.3,
+                    child: Image.asset(
+                      tractianLog,
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
