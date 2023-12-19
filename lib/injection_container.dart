dc274dea4248bdcb3b9b2e671569df78db09dbce
Gabriel Santos <gabrielsantostca@gmail.com>
2023-12-19 19:29:29 -0300

    feat: added the treeView bloc to the injection container

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -6,6 +6,7 @@ import 'package:tractian_challenge/domain/repositories/company_repository.dart';
 import 'package:tractian_challenge/domain/usecases/fetch_companies_usecase.dart';
 import 'package:tractian_challenge/domain/usecases/get_company_tree_components_usecase.dart';
 import 'package:tractian_challenge/presentation/companies/bloc/company_bloc.dart';
+import 'package:tractian_challenge/presentation/tree_view/pages/bloc/tree_view_bloc.dart';
 
 final locator = GetIt.instance;
 void setupLocator() {
@@ -17,4 +18,5 @@ void setupLocator() {
   locator.registerSingleton<GetCompanyTreeComponentsUseCase>(
       GetCompanyTreeComponentsUseCase(locator()));
   locator.registerFactory<CompanyBloc>(() => CompanyBloc(locator()));
+  locator.registerFactory<TreeViewBloc>(() => TreeViewBloc(locator()));
 }
