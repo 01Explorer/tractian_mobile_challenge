d80a3ef98b5c6485bfc1650680ddb5b64bf1c29a
Gabriel Santos <gabrielsantostca@gmail.com>
2023-12-19 20:10:07 -0300

    refactor: moved the files to the bloc folder

--- lib/presentation/tree_view/bloc/tree_view_bloc.dart
+++ lib/presentation/tree_view/bloc/tree_view_bloc.dart
@@ -1,7 +1,7 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:tractian_challenge/domain/usecases/get_company_tree_components_usecase.dart';
-import 'package:tractian_challenge/presentation/tree_view/pages/bloc/tree_view_event.dart';
-import 'package:tractian_challenge/presentation/tree_view/pages/bloc/tree_view_state.dart';
+import 'package:tractian_challenge/presentation/tree_view/bloc/tree_view_event.dart';
+import 'package:tractian_challenge/presentation/tree_view/bloc/tree_view_state.dart';
 
 class TreeViewBloc extends Bloc<TreeViewEvent, TreeViewState> {
   final GetCompanyTreeComponentsUseCase _getCompanyTreeComponentsUseCase;
