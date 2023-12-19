94a36584f716ba6e141e871c801c5e8b19978690
Gabriel Santos <gabrielsantostca@gmail.com>
2023-12-19 20:10:27 -0300

    fix: corrected the tree view bloc paths

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -6,7 +6,7 @@ import 'package:tractian_challenge/domain/repositories/company_repository.dart';
 import 'package:tractian_challenge/domain/usecases/fetch_companies_usecase.dart';
 import 'package:tractian_challenge/domain/usecases/get_company_tree_components_usecase.dart';
 import 'package:tractian_challenge/presentation/companies/bloc/company_bloc.dart';
-import 'package:tractian_challenge/presentation/tree_view/pages/bloc/tree_view_bloc.dart';
+import 'package:tractian_challenge/presentation/tree_view/bloc/tree_view_bloc.dart';
 
 final locator = GetIt.instance;
 void setupLocator() {
