ab37991aaa4e9270f8588eee9631e80fc93908ed
Gabriel Santos <gabrielsantostca@gmail.com>
2023-12-20 20:05:04 -0300

    feat: created the tree_view event class to work with filtering

--- lib/presentation/tree_view/bloc/tree_view_event.dart
+++ lib/presentation/tree_view/bloc/tree_view_event.dart
@@ -11,7 +11,35 @@ abstract class TreeViewEvent extends Equatable {
 class LoadTreeViewEvent extends TreeViewEvent {
   final CompanyEntity company;
 
-  const LoadTreeViewEvent({required this.company});
+  const LoadTreeViewEvent({
+    required this.company,
+  });
   @override
-  List<Object?> get props => [company, super.props];
+  List<Object?> get props => [
+        company,
+        super.props,
+      ];
+}
+
+class FilterTreeViewEvent extends TreeViewEvent {
+  final CompanyEntity company;
+  final String filter;
+  final bool filterEnergySensor;
+  final bool filterCriticalSensor;
+
+  const FilterTreeViewEvent({
+    required this.company,
+    this.filter = '',
+    this.filterEnergySensor = false,
+    this.filterCriticalSensor = false,
+  });
+
+  @override
+  List<Object?> get props => [
+        company,
+        filter,
+        filterEnergySensor,
+        filterCriticalSensor,
+        super.props,
+      ];
 }
