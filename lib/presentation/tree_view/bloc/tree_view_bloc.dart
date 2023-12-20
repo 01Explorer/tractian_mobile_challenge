ed70541ace66b4fa7df986165f090b65d7071cd4
Gabriel Santos <gabrielsantostca@gmail.com>
2023-12-20 20:06:21 -0300

    feat: implemented the logic to handle the filter event

--- lib/presentation/tree_view/bloc/tree_view_bloc.dart
+++ lib/presentation/tree_view/bloc/tree_view_bloc.dart
@@ -11,9 +11,40 @@ class TreeViewBloc extends Bloc<TreeViewEvent, TreeViewState> {
       emit(TreeViewLoading());
       final result =
           await _getCompanyTreeComponentsUseCase(companyEntity: event.company);
+
       result.fold(
         (failure) => emit(TreeViewFailure(message: failure.message)),
-        (rootAssets) => emit(TreeViewLoaded(rootAssets: rootAssets)),
+        (rootAssets) => emit(TreeViewLoaded(
+          rootAssets: rootAssets,
+        )),
+      );
+    });
+    on<FilterTreeViewEvent>((event, emit) async {
+      emit(TreeViewLoading());
+      final result =
+          await _getCompanyTreeComponentsUseCase(companyEntity: event.company);
+
+      result.fold(
+        (failure) => emit(TreeViewFailure(message: failure.message)),
+        (rootAssets) {
+          if (event.filterCriticalSensor) {
+            rootAssets = rootAssets.where((item) => item.isCritical).toList();
+          }
+          if (event.filterEnergySensor) {
+            rootAssets =
+                rootAssets.where((item) => item.isEletricSensor).toList();
+          }
+          if (event.filter.isNotEmpty) {
+            rootAssets = rootAssets
+                .where((item) => item.isBeingSearched(event.filter))
+                .toList();
+          }
+          emit(TreeViewLoaded(
+              rootAssets: rootAssets,
+              filter: event.filter,
+              isFilteredByCriticalSensor: event.filterCriticalSensor,
+              isFilteredByEnergySensor: event.filterEnergySensor));
+        },
       );
     });
   }
