3f021a56a8296f46b6d361ef8a5d7aa38a25a8f7
Gabriel Santos <gabrielsantostca@gmail.com>
2023-12-20 22:18:16 -0300

    feat: implemented an extension to highlight matching text

--- lib/core/helpers/extensions.dart
+++ lib/core/helpers/extensions.dart
@@ -1,5 +1,7 @@
 import 'dart:io';
 
+import 'package:flutter/material.dart';
+
 extension FileExtension on FileSystemEntity {
   String get name => path.split('\\').last;
 }
@@ -8,4 +10,41 @@ extension MapExtension on Map<String, dynamic> {
   bool get isComponent => this['status'] != null;
   bool get isAsset => !isComponent;
   bool get isRoot => this['parentId'] == null && this['locationId'] == null;
+}
+
+extension BoldSubString on Text {
+  Text boldSubString(String target) {
+    final textSpans = List.empty(growable: true);
+    final escapedTarget = RegExp.escape(target);
+    final pattern = RegExp(escapedTarget, caseSensitive: false);
+    final matches = pattern.allMatches(data!);
+
+    int currentIndex = 0;
+    for (final match in matches) {
+      final beforeMatch = data!.substring(currentIndex, match.start);
+      if (beforeMatch.isNotEmpty) {
+        textSpans.add(TextSpan(text: beforeMatch));
+      }
+
+      final matchedText = data!.substring(match.start, match.end);
+      textSpans.add(
+        TextSpan(
+          text: matchedText,
+          style: const TextStyle(
+              fontWeight: FontWeight.bold, backgroundColor: Colors.yellow),
+        ),
+      );
+
+      currentIndex = match.end;
+    }
+
+    if (currentIndex < data!.length) {
+      final remainingText = data!.substring(currentIndex);
+      textSpans.add(TextSpan(text: remainingText));
+    }
+
+    return Text.rich(
+      TextSpan(children: <TextSpan>[...textSpans]),
+    );
+  }
 }
