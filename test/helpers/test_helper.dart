57eda66acead36ed3a61898d2235a04921f239cd
Gabriel Santos <gabrielsantostca@gmail.com>
2023-12-21 00:19:48 -0300

    test: generated mock classes for both usecases

--- test/helpers/test_helper.dart
+++ test/helpers/test_helper.dart
@@ -1,9 +1,13 @@
 import 'package:mockito/annotations.dart';
 import 'package:tractian_challenge/data/datasources/local/local_datasource.dart';
 import 'package:tractian_challenge/domain/repositories/company_repository.dart';
+import 'package:tractian_challenge/domain/usecases/fetch_companies_usecase.dart';
+import 'package:tractian_challenge/domain/usecases/get_company_tree_components_usecase.dart';
 
 @GenerateMocks([
   CompanyRepository,
   LocalDataSource,
+  GetCompanyTreeComponentsUseCase,
+  FetchCompaniesUsecase,
 ])
 void main() {}
